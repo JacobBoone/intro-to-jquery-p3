3ab9d21a681db130bf2d732a298d8189a4fecf8e
Jacob <jakeboone@aol.com>
2014-06-27 12:34:36 -0600

    Complete

--- main.js
+++ main.js
@@ -1,3 +1,52 @@
 $(document).on('ready', function() {
   
+
+
+
+
+
+
+	$('#bigbutton').click(function(){
+
+		$('#bigbutton').before("<div class='lightbox'> <p class='subtitle'>Would you like to go on an Adventure?</p></div>")
+		$('.lightbox').append("<button class='close'>Mathmatical</button>")
+		console.log('This party is so crazy!');
+	
+
+		$('.close').click(function(){
+		$('.lightbox').remove();
+		console.log("closed")
+
+
+		});
+
 });
+
+
+
+		// $('.close').click(function(){
+		// // if(close === true){
+		// $('.lightbox').remove();
+		// console.log("closed")
+		// 	// }
+
+		// });
+		// $('.close').click(function()	
+
+
+	
+	// 	$('.lightbox').hidden()
+	
+
+
+
+
+
+
+
+});
+
+
+
+
+// <button class='close'>Mathmatical</button></div>
